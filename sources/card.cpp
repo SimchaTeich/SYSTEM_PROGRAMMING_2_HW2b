603c38304e509a73fe149d581bfe3b03e82504af
SimchaTeich <sim.teich@gmail.com>
2023-04-02 12:38:28 +0300

    implements Card class

--- sources/card.cpp
+++ sources/card.cpp
@@ -1,8 +1,35 @@
 #include "card.hpp"
+#include <iostream>
 
-namespace ariel
+
+ariel::Card::Card(int value, const string& type)
 {
-    Card::Card(int value, const string& color){};
-    //int Card::getValue() const{return 0;};
-    //void Card::printCard() const{};
+    this->_value = value;
+    this->_type = type;
+    //this->_specialName = "";
+
+    // init the special card name by value
+    if(ACE_VAL ==  this->_value){ this->_specialName = "Ace"; }
+    else if(PRINCE_VAL ==  this->_value){ this->_specialName = "Prince"; }
+    else if(QUEEN_VAL ==  this->_value){ this->_specialName = "Queen"; }
+    else if(KING_VAL ==  this->_value){ this->_specialName = "King"; }
+
+    // init the card color by type
+    if("Spades" == this->_type || "Clubs" == this->_type){ this->_color = "black"; }
+    else if("Hearts" == this->_type || "Diamonds" == this->_type){ this->_color = "red"; }
+};
+
+
+int ariel::Card::getValue() const
+{
+    return this->_value;
+};
+
+
+void ariel::Card::printCard() const
+{
+    std::cout << (this->_specialName != "" ? this->_specialName : std::to_string(this->_value))
+        << " of "
+        << this->_type
+        << std::endl;
 };
--- sources/card.hpp
+++ sources/card.hpp
@@ -3,23 +3,30 @@
 
 #include <string>
 
+#define ACE_VAL 1
+#define PRINCE_VAL 11
+#define QUEEN_VAL 12
+#define KING_VAL 13
+
 using std::string;
+
+
 
 namespace ariel
 {
     class Card
     {
-    // private:
-    //     int _value;
-    //     string _color;
-    //     string _type;
-    //     string _specialName;
+     private:
+         int _value;
+         string _color;
+         string _type;
+         string _specialName;
 
     public:
         Card(int value, const string& color);
 
-        //int getValue() const;
-        //void printCard() const;
+        int getValue() const;
+        void printCard() const;
     };
 };
 
