70974be5895113508708547fbd8130c22a1da78a
SimchaTeich <sim.teich@gmail.com>
2023-04-02 13:20:21 +0300

    implements Player class

--- sources/player.cpp
+++ sources/player.cpp
@@ -1,16 +1,78 @@
 #include "player.hpp"
+//#include <exception>
 
-namespace ariel
+
+ariel::Player::Player(const string& name)
 {
-    Player::Player(const string& name){};
-    //Player::~Player(){};
-    //void Player::insertCard(const Card& card){};
-    //Card Player::playCard(){};
-    //void Player::winTurn(int score, const string& card){};
-    //void Player::drawTurn(const string& card){};
-    //string Player::name() const{};
-    int Player::stacksize() const{return 0;};
-    int Player::cardesTaken() const{return 0;};
-    //queue<string> Player::cardsWasDraw() const{};
-    //queue<string> Player::cartdWasWin() const{};
+    if(name == "")
+    {
+        throw "Error: can't create player with no name..";
+    }
+    
+    this->_name = name;
+    this->_inPlay = false;
+    this->_cardsWasWin = 0;
+    this->_cardsWasDraw = 0;
+    this->_cardesTaken = 0;
 };
+
+
+void ariel::Player::insertCard(const Card& card)
+{
+    this->_inPlay = true;
+    this->_cardsStack.push(card);
+};
+
+
+ariel::Card ariel::Player::playCard()
+{
+    ariel::Card curr = this->_cardsStack.top();
+    this->_cardsStack.pop();
+    if(this->_cardsStack.empty()) { this->_inPlay = false; }
+
+    return curr;
+};
+
+
+void ariel::Player::winTurn(int score)
+{
+    this->_cardesTaken += score;
+    this->_cardsWasWin++;
+};
+
+
+void ariel::Player::drawTurn(int score)
+{
+    this->_cardesTaken += score;
+    this->_cardsWasDraw++;
+};
+
+
+string ariel::Player::name() const
+{
+    return this->_name;
+};
+
+
+int ariel::Player::stacksize() const
+{
+    return this->_cardsStack.size();
+};
+
+
+int ariel::Player::cardsWasWin() const
+{
+    return this->_cardsWasWin;
+};
+
+
+int ariel::Player::cardsWasDraw() const
+{
+    return this->_cardsWasDraw;
+};
+
+
+int ariel::Player::cardesTaken() const
+{
+    return this->_cardesTaken;
+}
--- sources/player.hpp
+++ sources/player.hpp
@@ -2,40 +2,39 @@
 #define PLAYER_HPP
 
 #include <string>
-//#include <queue>
-//#include <stack>
-//#include "card.hpp"
+#include <stack>
+#include "card.hpp"
 
 using std::string;
-//using std::queue;
-//using std::stack;
+using std::stack;
+
 
 namespace ariel
 {
     class Player
     {
-    // private:
-    //     string _name;
-    //     queue<Card> _cardsStack;     // act like queue but call "stack"..
-    //     queue<string> _cardsWasWin;
-    //     queue<string> _cardWasDraw;
-    //     int _cardesTaken;            // score
+    private:
+        string _name;
+        bool _inPlay;
+        stack<Card> _cardsStack;     // act like queue but call "stack"..
+        int _cardsWasWin;
+        int _cardsWasDraw;
+        int _cardesTaken;            // score
 
     public:
         Player(const string& name);
-        //~Player();
 
-        //void insertCard(const Card& card);
-        //Card playCard();
-        //void winTurn(int score, const string& card);  // append score to _cardesTaken
-        //void drawTurn(const string& card);            // updates _cardWasDraw
+        void insertCard(const Card& card);
+        Card playCard();
+        void winTurn(int score);    // append score to _cardesTaken and add 1 to _cardsWasWin 
+        void drawTurn(int score);   // append score to _cardesTaken and add 1 to _cardWasDraw
 
         // getters
-        //string name() const;
+        string name() const;
         int stacksize() const;
+        int cardsWasWin() const;
+        int cardsWasDraw() const;
         int cardesTaken() const;
-        //queue<string> cardsWasDraw() const;
-        //queue<string> cartdWasWin() const;
     };
 };
 
