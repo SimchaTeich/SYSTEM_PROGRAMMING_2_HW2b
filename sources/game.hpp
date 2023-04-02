586ba58a9bd9be11fb5943c4fc47bad53b92d863
SimchaTeich <sim.teich@gmail.com>
2023-04-02 14:49:54 +0300

    update headers of Game class

--- sources/game.hpp
+++ sources/game.hpp
@@ -1,35 +1,36 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
-//#include <string>
-//#include <stack>
-//#include <queue>
+#include <string>
+#include <stack>
+#include <queue>
 #include "player.hpp"
-//#include "card.hpp"
+#include "card.hpp"
 
-//using std::string;
-//using std::stack;
-//using std::queue;
+using std::string;
+using std::stack;
+using std::queue;
 
 namespace ariel
 {
     class Game
     {
-    // private:
-    //     Player _player1;
-    //     Player _player2;
-    //     stack<Card> _cardsStack;
-    //     bool _gameIsOver;
-    //     int _winner;               // 0, 1 or 2.
-    //     queue<string> _logOfTurns;
+    private:
+        Player _player1;
+        Player _player2;
+        stack<Card> _cardsStack;
+        int _turnNum;               // turns counter
+        bool _gameIsOver;
+        int _winner;               // 0, 1 or 2.
+        queue<string> _logOfTurns;
 
-    //    void initCards();
-    //    void divideCardsToPlayers();
-    //    void writeTurnLog(const Card& c1, const Card& c2);
+        void initCards();
+        void createAllCards();
+        void divideCardsToPlayers();
+        void writeTurnLog(string newLog);
 
     public:
         Game(Player& player1, Player& player2);
-    //    ~Game();
 
         void playTurn();
         void playAll();
