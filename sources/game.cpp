48f68b3a192a05c5ce55cc4a09c76cec2084e7d2
SimchaTeich <sim.teich@gmail.com>
2023-04-02 14:56:16 +0300

    implements Game constructor

--- sources/game.cpp
+++ sources/game.cpp
@@ -1,20 +1,21 @@
 #include "game.hpp"
 
-//using std::string;
 
-namespace ariel
+ariel::Game::Game(Player& player1, Player& player2)
+    : _player1(player1), _player2(player2)
 {
-    Game::Game(Player& player1, Player& player2){};
-    //Game::~Game(){};
-    void Game::playTurn(){};
-    void Game::playAll(){};
-    void Game::printLastTurn() const {};
-    void Game::printLog() const{};
-    void Game::printWiner() const{};
-    void Game::printStats() const{};
-
-    //  helpers
-    //void Game::initCards(){};
-    //void Game::divideCardsToPlayers(){};
-    //void Game::writeTurnLog(const Card& c1, const Card& c2){};
 };
+
+
+void ariel::Game::playTurn(){};
+void ariel::Game::playAll(){};
+void ariel::Game::printLastTurn() const {};
+void ariel::Game::printLog() const{};
+void ariel::Game::printWiner() const{};
+void ariel::Game::printStats() const{};
+
+
+//  helpers
+//void Game::initCards(){};
+//void Game::divideCardsToPlayers(){};
+//void Game::writeTurnLog(const Card& c1, const Card& c2){};
