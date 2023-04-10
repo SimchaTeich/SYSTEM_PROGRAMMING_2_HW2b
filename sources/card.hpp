c60c3c241a3ead4df5bb635d3e6fe0fde47a4d60
SimchaTeich <sim.teich@gmail.com>
2023-04-10 16:20:38 +0300

    document the code

--- sources/card.hpp
+++ sources/card.hpp
@@ -22,10 +22,42 @@ namespace ariel
          string _specialName;
 
     public:
+        /*
+        The C'TOR.
+        Input: value from 1 to 13,
+               string type ("Spades", "Clubs",
+               "Hearts" or "Diamonds")
+        */
         Card(int value, const string& type);
 
+
+        /*
+        Return the value of the card.
+        Posible value is from 1 to 13.
+        Input: void
+        Output: the integer value as exaplain abouv.
+        */
         int getValue() const;
+
+
+        /*
+        Make a string format of the card,
+        and returns it.
+        Example 1: "8 of Spades"
+        Example 2: "King of Diamonds"
+        Example 3: "Ace of Hearts"
+        Input: void
+        Output: the string as exaplain abouv
+        */
         string cardString() const;
+
+
+        /*
+        Just printing the card in format of
+        function cardString.
+        Input: void
+        Output: void
+        */
         void printCard() const;
     };
 };
